2c25e2a413a1f7561b695e568baadf3b1cddbc28
locemarn <marcelon27@gmail.com>
2018-03-10 23:08:10 -0300

    admin template

---
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -0,0 +1,38 @@
+<!-- Left side column. contains the logo and sidebar -->
+<aside class="main-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+      <!-- Sidebar user panel -->
+      <div class="user-panel">
+        <div class="pull-left image">
+          <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+        </div>
+        <div class="pull-left info">
+          <p>Marcelo Nogueira</p>
+        </div>
+      </div>
+      <!-- search form -->
+      <form action="#" method="get" class="sidebar-form">
+        <div class="input-group">
+          <input type="text" name="q" class="form-control" placeholder="Search...">
+          <span class="input-group-btn">
+                <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
+                </button>
+              </span>
+        </div>
+      </form>
+      <!-- /.search form -->
+      <!-- sidebar menu: : style can be found in sidebar.less -->
+      <ul class="sidebar-menu" data-widget="tree">
+        <li class="header">MAIN NAVIGATION</li>
+        <li class="active treeview">
+            <li class=""><a href="index.html"><i class="fa fa-circle-o"></i> Posts</a></li>
+            <li class=""><a href="index.html"><i class="fa fa-circle-o"></i> Category</a></li>
+            <li class=""><a href="index.html"><i class="fa fa-circle-o"></i> Tags</a></li>
+            <li class=""><a href="index.html"><i class="fa fa-circle-o"></i> Users</a></li>
+        </li>
+        
+      </ul>
+    </section>
+    <!-- /.sidebar -->
+  </aside>
