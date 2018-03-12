dd58094227f7f86675be92783b184591991147cc
locemarn <marcelon27@gmail.com>
2018-03-12 13:02:18 -0300

    Add Facebook comments system #16

--- resources/views/user/post.blade.php
+++ resources/views/user/post.blade.php
@@ -6,6 +6,14 @@
 
 @section('main-content')
 <!-- Post Content -->
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v2.12&appId=356223664894518&autoLogAppEvents=1';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
 <article>
   <div class="container">
     <div class="row">
@@ -22,10 +30,14 @@
         {{--  Tag clouds  --}}
         <h3>Tag Clouds</h3>
         @foreach ($post->tags as $tag)
-            <small class="pull-left" style="margin-right: 20px; border-radius:5px; border:1px solid gray; padding: 3px;background:gray; color:#fff;">
-              {{ $tag->name }}
-            </small>      
-          @endforeach
+          <small class="pull-left" style="margin-right: 20px; border-radius:5px; border:1px solid gray; padding: 3px;background:gray; color:#fff;">
+            {{ $tag->name }}
+          </small>      
+        @endforeach
+      </div>
+      <hr>
+      <div class=" col-lg-8 col-md-10 mx-auto" style="margin-top:100px;">
+        <div class="fb-comments" data-href="{{ Request::url() }}" data-numposts="10"></div>
       </div>
     </div>
   </div>
